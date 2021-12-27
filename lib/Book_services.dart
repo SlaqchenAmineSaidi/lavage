0bf7a93b82b353195d03816aca17d79a44050ea4
slaqchen <ami0190ne@gmail.com>
2021-12-27 20:11:35 +0100

    second

--- lib/Book_services.dart
+++ lib/Book_services.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 class BookService extends StatefulWidget {
   const BookService({Key key}) : super(key: key);
@@ -12,51 +11,99 @@ class _BookServiceState extends State<BookService> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
         title: Text('Washing Car'),
       ),
       body: Column(
         children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 20.0),
+            child: Container(
+              color: Colors.white,
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Material(
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => BookService()));
+                      },
+                      child: Icon(
+                        Icons.drive_eta,
+                        size: 70.0,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 30.0,
+                  ),
+                  Icon(
+                    Icons.motorcycle_outlined,
+                    size: 70.0,
+                  ),
+                  SizedBox(
+                    width: 30.0,
+                  ),
+                  Icon(
+                    Icons.airport_shuttle_rounded,
+                    size: 70.0,
+                  ),
+                  SizedBox(
+                    width: 30.0,
+                  ),
+                  Icon(
+                    Icons.electric_rickshaw,
+                    size: 70.0,
+                  )
+                ],
+              ),
+            ),
+          ),
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Icon(
-                    Icons.car_repair,
-                    size: 100.0,
+            child: Container(
+              color: Colors.white,
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 25.0,
                   ),
-                ),
-                Icon(
-                  Icons.motorcycle_outlined,
-                  size: 100.0,
-                ),
-                SizedBox(
-                  width: 8.0,
-                ),
-                Icon(
-                  Icons.airport_shuttle_rounded,
-                  size: 100.0,
-                ),
-                SizedBox(
-                  width: 8.0,
-                ),
-                Icon(
-                  Icons.bus_alert,
-                  size: 100.0,
-                ),
-                SizedBox(
-                  width: 8.0,
-                ),
-                Icon(
-                  Icons.electric_rickshaw,
-                  size: 100.0,
-                )
-              ],
+                  Text(
+                    'Car',
+                    style: TextStyle(color: Colors.cyan[200], fontSize: 20.0),
+                  ),
+                  SizedBox(
+                    width: 50.0,
+                  ),
+                  Text(
+                    'Motor',
+                    style: TextStyle(color: Colors.cyan[200], fontSize: 20.0),
+                  ),
+                  SizedBox(
+                    width: 60.0,
+                  ),
+                  Text(
+                    'Bus',
+                    style: TextStyle(color: Colors.cyan[200], fontSize: 20.0),
+                  ),
+                  SizedBox(
+                    width: 40.0,
+                  ),
+                  Text(
+                    'Electric_rickshaw',
+                    style: TextStyle(color: Colors.cyan[200], fontSize: 20.0),
+                  ),
+                ],
+              ),
             ),
-          )
+          ),
         ],
       ),
     );
