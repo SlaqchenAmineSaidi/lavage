3b247eefd955935b4e8e1a6d74ffad62ce7c45fa
slaqchen <ami0190ne@gmail.com>
2021-12-28 12:23:55 +0100

    second

--- lib/Book_services.dart
+++ lib/Book_services.dart
@@ -14,10 +14,15 @@ class _BookServiceState extends State<BookService> {
   DateTime _focusedDay = DateTime.now();
   DateTime _selectedDay;
   List<bool> isSelected;
-
+  List<bool> _isSelected1;
+  List<bool> _isSelected2;
+  List<bool> _isSelected3;
   @override
   void initState() {
     isSelected = [false, false, false, false];
+    _isSelected1 = [false, false, false, false];
+    _isSelected2 = [false, false, false, false];
+    _isSelected3 = [false, false, false, false];
     super.initState();
   }
 
@@ -28,110 +33,215 @@ class _BookServiceState extends State<BookService> {
       appBar: AppBar(
         title: Text('Washing Car'),
       ),
-      body: Column(
-        children: [
-          Text('Choose the gender',
-              style: TextStyle(color: Colors.cyan[200], fontSize: 20.0)),
-          Padding(
-            padding: const EdgeInsets.only(top: 8.0, right: 15.0, left: 15.0),
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: ToggleButtons(
-                borderColor: Colors.black,
-                fillColor: Colors.grey,
-                borderWidth: 2,
-                selectedBorderColor: Colors.cyanAccent,
-                selectedColor: Colors.white,
-                borderRadius: BorderRadius.circular(0),
-                children: [
-                  Icon(
-                    Icons.drive_eta,
-                    size: 90.0,
-                  ),
-                  Icon(
-                    Icons.motorcycle_outlined,
-                    size: 90.0,
-                  ),
-                  Icon(
-                    Icons.airport_shuttle_rounded,
-                    size: 90.0,
-                  ),
-                  Icon(
-                    Icons.electric_rickshaw,
-                    size: 90.0,
-                  )
-                ],
-                onPressed: (int index) {
-                  setState(() {
-                    for (int buttonIndex = 0;
-                        buttonIndex < isSelected.length;
-                        buttonIndex++) {
-                      if (buttonIndex == index) {
-                        isSelected[buttonIndex] = !isSelected[buttonIndex];
-                      } else {
-                        isSelected[buttonIndex] = false;
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Text('Choose the gender',
+                style: TextStyle(color: Colors.cyan[200], fontSize: 20.0)),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0, right: 15.0, left: 15.0),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: ToggleButtons(
+                  borderColor: Colors.black,
+                  fillColor: Colors.grey,
+                  borderWidth: 2,
+                  selectedBorderColor: Colors.cyanAccent,
+                  selectedColor: Colors.white,
+                  borderRadius: BorderRadius.circular(0),
+                  children: [
+                    Icon(
+                      Icons.drive_eta,
+                      size: 90.0,
+                    ),
+                    Icon(
+                      Icons.motorcycle_outlined,
+                      size: 90.0,
+                    ),
+                    Icon(
+                      Icons.airport_shuttle_rounded,
+                      size: 90.0,
+                    ),
+                    Icon(
+                      Icons.electric_rickshaw,
+                      size: 90.0,
+                    )
+                  ],
+                  onPressed: (int index) {
+                    setState(() {
+                      for (int buttonIndex = 0;
+                          buttonIndex < isSelected.length;
+                          buttonIndex++) {
+                        if (buttonIndex == index) {
+                          isSelected[buttonIndex] = !isSelected[buttonIndex];
+                        } else {
+                          isSelected[buttonIndex] = false;
+                        }
                       }
-                    }
-                  });
-                },
-                isSelected: isSelected,
+                    });
+                  },
+                  isSelected: isSelected,
+                ),
               ),
             ),
-          ),
-          // ToggleButtons(children: [
-          //   Text(
-          //     'Car',
-          //     style: TextStyle(color: Colors.cyan[200], fontSize: 20.0),
-          //   ),
-          //   Text(
-          //     'Motor',
-          //     style: TextStyle(color: Colors.cyan[200], fontSize: 20.0),
-          //   ),
-          //   Text(
-          //     'Bus',
-          //     style: TextStyle(color: Colors.cyan[200], fontSize: 20.0),
-          //   ),
-          //   Text(
-          //     'Electric_rickshaw',
-          //     style: TextStyle(color: Colors.cyan[200], fontSize: 20.0),
-          //   ),
-          // ], isSelected: isSelected),
-          TableCalendar(
-            firstDay: DateTime.now(),
-            lastDay: DateTime.utc(2023, 10, 16),
-            focusedDay: _focusedDay,
-            calendarFormat: _calendarFormat,
-            selectedDayPredicate: (day) {
-              // Use `selectedDayPredicate` to determine which day is currently selected.
-              // If this returns true, then `day` will be marked as selected.
+            // ToggleButtons(children: [
+            //   Text(
+            //     'Car',
+            //     style: TextStyle(color: Colors.cyan[200], fontSize: 20.0),
+            //   ),
+            //   Text(
+            //     'Motor',
+            //     style: TextStyle(color: Colors.cyan[200], fontSize: 20.0),
+            //   ),
+            //   Text(
+            //     'Bus',
+            //     style: TextStyle(color: Colors.cyan[200], fontSize: 20.0),
+            //   ),
+            //   Text(
+            //     'Electric_rickshaw',
+            //     style: TextStyle(color: Colors.cyan[200], fontSize: 20.0),
+            //   ),
+            // ], isSelected: isSelected),
+            TableCalendar(
+              firstDay: DateTime.now(),
+              lastDay: DateTime.utc(2023, 10, 16),
+              focusedDay: _focusedDay,
+              calendarFormat: _calendarFormat,
+              selectedDayPredicate: (day) {
+                // Use `selectedDayPredicate` to determine which day is currently selected.
+                // If this returns true, then `day` will be marked as selected.
 
-              // Using `isSameDay` is recommended to disregard
-              // the time-part of compared DateTime objects.
-              return isSameDay(_selectedDay, day);
-            },
-            onDaySelected: (selectedDay, focusedDay) {
-              if (!isSameDay(_selectedDay, selectedDay)) {
-                // Call `setState()` when updating the selected day
+                // Using `isSameDay` is recommended to disregard
+                // the time-part of compared DateTime objects.
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                if (!isSameDay(_selectedDay, selectedDay)) {
+                  // Call `setState()` when updating the selected day
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                  });
+                }
+              },
+              onFormatChanged: (format) {
+                if (_calendarFormat != format) {
+                  // Call `setState()` when updating calendar format
+                  setState(() {
+                    _calendarFormat = format;
+                  });
+                }
+              },
+              onPageChanged: (focusedDay) {
+                // No need to call `setState()` here
+                _focusedDay = focusedDay;
+              },
+            ),
+            ToggleButtons(
+              children: [
+                Text(
+                  '     09:00 AM     ',
+                  style: TextStyle(color: Colors.blueGrey),
+                ),
+                Text(
+                  '     10:00 AM     ',
+                  style: TextStyle(color: Colors.blueGrey),
+                ),
+                Text(
+                  '     11:00 AM     ',
+                  style: TextStyle(color: Colors.blueGrey),
+                ),
+                Text(
+                  '     12:00 AM     ',
+                  style: TextStyle(color: Colors.blueGrey),
+                ),
+              ],
+              isSelected: _isSelected1,
+              onPressed: (int index) {
                 setState(() {
-                  _selectedDay = selectedDay;
-                  _focusedDay = focusedDay;
+                  for (int buttonIndex = 0;
+                      buttonIndex < _isSelected1.length;
+                      buttonIndex++) {
+                    if (buttonIndex == index) {
+                      _isSelected1[buttonIndex] = !_isSelected1[buttonIndex];
+                    } else {
+                      _isSelected1[buttonIndex] = false;
+                    }
+                  }
                 });
-              }
-            },
-            onFormatChanged: (format) {
-              if (_calendarFormat != format) {
-                // Call `setState()` when updating calendar format
+              },
+            ),
+            ToggleButtons(
+              children: [
+                Text(
+                  '     01:00 PM     ',
+                  style: TextStyle(color: Colors.blueGrey),
+                ),
+                Text(
+                  '     02:00 PM     ',
+                  style: TextStyle(color: Colors.blueGrey),
+                ),
+                Text(
+                  '     03:00 PM     ',
+                  style: TextStyle(color: Colors.blueGrey),
+                ),
+                Text(
+                  '     04:00 PM     ',
+                  style: TextStyle(color: Colors.blueGrey),
+                ),
+              ],
+              isSelected: _isSelected2,
+              onPressed: (int index) {
                 setState(() {
-                  _calendarFormat = format;
+                  for (int buttonIndex = 0;
+                      buttonIndex < _isSelected2.length;
+                      buttonIndex++) {
+                    if (buttonIndex == index) {
+                      _isSelected2[buttonIndex] = !_isSelected2[buttonIndex];
+                    } else {
+                      _isSelected2[buttonIndex] = false;
+                    }
+                  }
                 });
-              }
-            },
-            onPageChanged: (focusedDay) {
-              // No need to call `setState()` here
-              _focusedDay = focusedDay;
-            },
-          ),
-        ],
+              },
+            ),
+            ToggleButtons(
+              children: [
+                Text(
+                  '     05:00 PM     ',
+                  style: TextStyle(color: Colors.blueGrey),
+                ),
+                Text(
+                  '     06:00 PM     ',
+                  style: TextStyle(color: Colors.blueGrey),
+                ),
+                Text(
+                  '     07:00 PM     ',
+                  style: TextStyle(color: Colors.blueGrey),
+                ),
+                Text(
+                  '     08:00 PM     ',
+                  style: TextStyle(color: Colors.blueGrey),
+                ),
+              ],
+              isSelected: _isSelected3,
+              onPressed: (int index) {
+                setState(() {
+                  for (int buttonIndex = 0;
+                      buttonIndex < _isSelected3.length;
+                      buttonIndex++) {
+                    if (buttonIndex == index) {
+                      _isSelected3[buttonIndex] = !_isSelected3[buttonIndex];
+                    } else {
+                      _isSelected3[buttonIndex] = false;
+                    }
+                  }
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
