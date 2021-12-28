e73fed6bbfa0ac492f8ef0293226a7e82eb46517
slaqchen <ami0190ne@gmail.com>
2021-12-28 15:07:51 +0100

    second

--- lib/Book_services.dart
+++ lib/Book_services.dart
@@ -1,5 +1,6 @@
 // import 'dart:collection';
 import 'package:flutter/material.dart';
+import 'package:flutter_laravel/Wash_services.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class BookService extends StatefulWidget {
@@ -17,6 +18,15 @@ class _BookServiceState extends State<BookService> {
   List<bool> _isSelected1;
   List<bool> _isSelected2;
   List<bool> _isSelected3;
+  final ButtonStyle flatButtonStyle = TextButton.styleFrom(
+    primary: Colors.black87,
+    minimumSize: Size(110, 50),
+    padding: EdgeInsets.symmetric(horizontal: 16.0),
+    backgroundColor: Colors.indigo[400],
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(6.0)),
+    ),
+  );
   @override
   void initState() {
     isSelected = [false, false, false, false];
@@ -37,40 +47,48 @@ class _BookServiceState extends State<BookService> {
         scrollDirection: Axis.vertical,
         child: Column(
           children: [
-            Text('Choose the gender',
-                style: TextStyle(
-                  color: Colors.cyan[200],
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
+            Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  margin: EdgeInsets.only(right: 100, left: 100),
+                  padding: EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.lightBlue,
+                      border: Border.all(
+                          color: Colors.blue, // Set border color
+                          width: 3.0), // Set border width
+                      borderRadius: BorderRadius.all(
+                          Radius.circular(10.0)), // Set rounded corner radius
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 10,
+                            color: Colors.black,
+                            offset: Offset(1, 3))
+                      ] // Make rounded corner of border
+                      ),
+                  child: Text(
+                    "Choose your gender",
+                    style: TextStyle(color: Colors.white, fontSize: 18.0),
+                  ),
                 )),
             Padding(
-              padding: const EdgeInsets.only(top: 8.0, right: 15.0, left: 15.0),
+              padding:
+                  const EdgeInsets.only(top: 12.0, right: 15.0, left: 15.0),
               child: SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: ToggleButtons(
                   borderColor: Colors.black,
-                  fillColor: Colors.grey,
+                  fillColor: Colors.white,
                   borderWidth: 2,
-                  selectedBorderColor: Colors.cyanAccent,
+                  selectedBorderColor: Colors.blue,
                   selectedColor: Colors.white,
                   borderRadius: BorderRadius.circular(0),
                   children: [
-                    Icon(
-                      Icons.drive_eta,
-                      size: 90.0,
-                    ),
-                    Icon(
-                      Icons.motorcycle_outlined,
-                      size: 90.0,
-                    ),
-                    Icon(
-                      Icons.airport_shuttle_rounded,
-                      size: 90.0,
-                    ),
-                    Icon(
-                      Icons.electric_rickshaw,
-                      size: 90.0,
-                    )
+                    Image.asset('images/caar.png', width: 100.0),
+                    Image.asset('images/buus.PNG', width: 100.0),
+                    Image.asset('images/truck_logo.jpg', width: 100.0),
+                    Image.asset('images/moto_logo.png', width: 100.0),
                   ],
                   onPressed: (int index) {
                     setState(() {
@@ -90,11 +108,30 @@ class _BookServiceState extends State<BookService> {
                 ),
               ),
             ),
-            Text('Select Date & Time',
-                style: TextStyle(
-                  color: Colors.cyan[200],
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
+            Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Container(
+                  margin: EdgeInsets.only(right: 100, left: 100),
+                  padding: EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.lightBlue,
+                      border: Border.all(
+                          color: Colors.blue, // Set border color
+                          width: 3.0), // Set border width
+                      borderRadius: BorderRadius.all(
+                          Radius.circular(10.0)), // Set rounded corner radius
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 10,
+                            color: Colors.black,
+                            offset: Offset(1, 3))
+                      ] // Make rounded corner of border
+                      ),
+                  child: Text(
+                    "Select Date & Time",
+                    style: TextStyle(color: Colors.white, fontSize: 18.0),
+                  ),
                 )),
             TableCalendar(
               firstDay: DateTime.now(),
@@ -131,39 +168,65 @@ class _BookServiceState extends State<BookService> {
                 _focusedDay = focusedDay;
               },
             ),
-            ToggleButtons(
-              children: [
-                Text(
-                  '     09:00 AM     ',
-                  style: TextStyle(color: Colors.blueGrey),
-                ),
-                Text(
-                  '     10:00 AM     ',
-                  style: TextStyle(color: Colors.blueGrey),
-                ),
-                Text(
-                  '     11:00 AM     ',
-                  style: TextStyle(color: Colors.blueGrey),
-                ),
-                Text(
-                  '     12:00 AM     ',
-                  style: TextStyle(color: Colors.blueGrey),
-                ),
-              ],
-              isSelected: _isSelected1,
-              onPressed: (int index) {
-                setState(() {
-                  for (int buttonIndex = 0;
-                      buttonIndex < _isSelected1.length;
-                      buttonIndex++) {
-                    if (buttonIndex == index) {
-                      _isSelected1[buttonIndex] = !_isSelected1[buttonIndex];
-                    } else {
-                      _isSelected1[buttonIndex] = false;
+            Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Container(
+                  child: Text("12 Slots"),
+                  margin: EdgeInsets.only(right: 140, left: 140),
+                  padding: EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.orange,
+                      border: Border.all(
+                          color: Colors.pink[800], // Set border color
+                          width: 3.0), // Set border width
+                      borderRadius: BorderRadius.all(
+                          Radius.circular(10.0)), // Set rounded corner radius
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 10,
+                            color: Colors.black,
+                            offset: Offset(1, 3))
+                      ] // Make rounded corner of border
+                      ),
+                )),
+            Padding(
+              padding: const EdgeInsets.all(0.0),
+              child: ToggleButtons(
+                children: [
+                  Text(
+                    '     09:00 AM     ',
+                    style: TextStyle(color: Colors.blueGrey),
+                  ),
+                  Text(
+                    '     10:00 AM     ',
+                    style: TextStyle(color: Colors.blueGrey),
+                  ),
+                  Text(
+                    '     11:00 AM     ',
+                    style: TextStyle(color: Colors.blueGrey),
+                  ),
+                  Text(
+                    '     12:00 AM     ',
+                    style: TextStyle(color: Colors.blueGrey),
+                  ),
+                ],
+                borderColor: Colors.black,
+                isSelected: _isSelected1,
+                onPressed: (int index) {
+                  setState(() {
+                    for (int buttonIndex = 0;
+                        buttonIndex < _isSelected1.length;
+                        buttonIndex++) {
+                      if (buttonIndex == index) {
+                        _isSelected1[buttonIndex] = !_isSelected1[buttonIndex];
+                      } else {
+                        _isSelected1[buttonIndex] = false;
+                      }
                     }
-                  }
-                });
-              },
+                  });
+                },
+              ),
             ),
             ToggleButtons(
               children: [
@@ -185,6 +248,7 @@ class _BookServiceState extends State<BookService> {
                 ),
               ],
               isSelected: _isSelected2,
+              borderColor: Colors.black,
               onPressed: (int index) {
                 setState(() {
                   for (int buttonIndex = 0;
@@ -219,6 +283,7 @@ class _BookServiceState extends State<BookService> {
                 ),
               ],
               isSelected: _isSelected3,
+              borderColor: Colors.black,
               onPressed: (int index) {
                 setState(() {
                   for (int buttonIndex = 0;
@@ -233,6 +298,21 @@ class _BookServiceState extends State<BookService> {
                 });
               },
             ),
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Container(
+                child: TextButton(
+                  style: flatButtonStyle,
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => Wash_services()));
+                  },
+                  child: Text('Next'),
+                ),
+              ),
+            )
           ],
         ),
       ),
---
+++ lib/Wash_services.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Wash_services extends StatefulWidget {
+  const Wash_services({Key key}) : super(key: key);
+
+  @override
+  _Wash_servicesState createState() => _Wash_servicesState();
+}
+
+class _Wash_servicesState extends State<Wash_services> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
